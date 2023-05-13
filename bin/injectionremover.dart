9b4a268203b1a7b1c21ecf4c042916310d62fd70
AWM <77528909+awholemystery@users.noreply.github.com>
2023-05-14 00:18:20 +0200

    french spelling fixes
    sinon le programme est vraiment bien !

--- bin/injectionremover.dart
+++ bin/injectionremover.dart
@@ -13,15 +13,15 @@ void main() {
     Directory("./file").createSync();
   }
   String? local = Platform.environment["LOCALAPPDATA"]!;
-  print("\x1b[34mCe Programme Redémarreras Votre/vos Discord(s), Voulez Vous Continuez (y/n) ?:\x1b[0m ");
+  print("\x1b[34mCe programme redémarrera votre/vos Discord(s), voulez-vous continuer (y/n) ?:\x1b[0m ");
   var b = stdin.readLineSync();
   if (b == "y" || b == "Y") {
     main2(local, DiscordContent, regex);
   } else if (b == "n") {
-    print("\x1b[34mFermeture Du Programme.\x1b[0m");
+    print("\x1b[34mFermeture du programme.\x1b[0m");
     exit(0);
   } else {
-    print("\x1b[35mRépondez Par y ou n\x1b[0m");
+    print("\x1b[35mRépondez par y ou n.\x1b[0m");
     exit(1);
   }
 }
@@ -30,7 +30,7 @@ void main2(String local, String DiscordContent, List<RegExp> regex) {
   Directory(local).listSync().forEach((entry) {
     if (entry.path.contains("iscord")) {
       var path = "${entry.path}";
-      print("\x1b[32m${entry.path.split('/').last} Trouver ! About To Check...\x1b[0m");
+      print("\x1b[32m${entry.path.split('/').last} trouvé ! Vérification en cours...\x1b[0m");
       check(path, DiscordContent, regex);
     }
   });
@@ -54,12 +54,12 @@ void check(String path, String DiscordContent, List<RegExp> regex) {
       var n = File(path).readAsStringSync();
       if (n != DiscordContent) {
         File(path).copySync("./file/${Discord}_index.js");
-        print("\x1b[31mLe Contenu De $Discord est suspect, veuillez regarder dans /file/${Discord}_index.js\x1b[0m\n\x1b[34mChangement De L'index..\x1b[0m");
+        print("\x1b[31mLe contenu de $Discord est suspect, veuillez regarder dans /file/${Discord}_index.js\x1b[0m\n\x1b[34mChangement de l'index..\x1b[0m");
         File(path).writeAsStringSync(DiscordContent);
-        print("\x1b[32m$Discord Rendu Safe !\nRedémarrage De $Discord En Cours....\x1b[0m");
+        print("\x1b[32m$Discord rendu sûr !\nRedémarrage De $Discord en cours....\x1b[0m");
         redem(Discord, local);
       } else {
-        print("\x1b[32mLe Contenu De $Discord est normal :D\x1b[0m");
+        print("\x1b[32mLe contenu de $Discord est normal :D\x1b[0m");
       }
     });
   });
@@ -70,8 +70,8 @@ void redem(String Discord, String local) {
   if (killList.contains("$Discord.exe")) {
     Process.runSync("taskkill", ["/IM", "$Discord.exe", "/F"]);
   } else {
-    print("\x1b[32m$Discord n'est pas présent Dans Le Tasklist..\x1b[0m");
+    print("\x1b[32m$Discord n'est pas présent dans la liste des processus..\x1b[0m");
   }
-  print("\x1b[33mDémarrage De $Discord\x1b[0m");
+  print("\x1b[33mDémarrage de $Discord\x1b[0m");
   Process.runSync("${local}/${Discord}/Update.exe", ["--processStart", "$Discord.exe"]);
 }
